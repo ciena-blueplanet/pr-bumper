9da6ced00c8a312418c0ca0e30be3ce2c1f30ceb
Adam Meadows <adam.meadows@gmail.com>
2017-02-09 11:13:46 -0700

    Try to speed up the extra builds

---
+++ .travis/is-bump-commit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+isBumpCommit() {
+  msg=`git log --pretty=format:'%s' -1`
+  if [ "$msg" = "Automated version bump" ]
+  then
+    # totally un-intuitive, but returning 0 evaluates to true when put in a if
+    # http://stackoverflow.com/a/5431932
+    return 0
+  else
+    # totally un-intuitive, but returning 1 evaluates to false when put in a if
+    # http://stackoverflow.com/a/5431932
+    return 1
+  fi
+}
---
+++ .travis/maybe-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source .travis/is-bump-commit.sh
+
+if isBumpCommit
+then
+  echo "Skipping install step for version bump commit"
+  exit 0
+fi
+
+npm test
