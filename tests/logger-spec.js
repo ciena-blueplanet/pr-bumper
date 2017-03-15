5cd7136f7a8183f285b4d2ba36101d14d20ca874
Adam Meadows <adam.meadows@gmail.com>
2017-03-15 04:37:10 -0600

    Cleaned up logging messages and fixed cli

--- tests/logger-spec.js
+++ tests/logger-spec.js
@@ -6,26 +6,96 @@ const sinonChai = require('sinon-chai')
 const expect = chai.expect
 chai.use(sinonChai)
 
-const pkgJson = require('../package.json')
+const name = require('../package.json').name
 const logger = require('../lib/logger')
 
 describe('logger', function () {
-  let stub
-  describe('.log()', function () {
-    it('should log to console', function () {
-      stub = sinon.stub(console, 'log')
-      logger.log('foo bar baz')
-      expect(console.log).to.have.been.calledWith(`${pkgJson.name}: foo bar baz`)
+  let stub, realVerbose
+
+  beforeEach(function () {
+    realVerbose = process.env['VERBOSE']
+  })
+
+  afterEach(function () {
+    process.env['VERBOSE'] = realVerbose
+    if (stub) {
       stub.restore()
+    }
+  })
+
+  describe('.log()', function () {
+    describe('when VERBOSE is not in env', function () {
+      beforeEach(function () {
+        delete process.env['VERBOSE']
+      })
+
+      describe('when force is not given', function () {
+        // NOTE: we do not want to stub console.log in beforeEach b/c it messes with mocha's reporting
+
+        it('should not log to console', function () {
+          stub = sinon.stub(console, 'log')
+          logger.log('foo bar baz')
+          expect(console.log).to.have.callCount(0)
+        })
+      })
+
+      describe('when force is given', function () {
+        it('should log to console', function () {
+          stub = sinon.stub(console, 'log')
+          logger.log('foo bar baz', true)
+          expect(console.log).to.have.been.calledWith(`${name}: foo bar baz`)
+        })
+      })
+    })
+
+    describe('when VERBOSE is in env', function () {
+      beforeEach(function () {
+        process.env['VERBOSE'] = '1'
+      })
+
+      describe('when force is not given', function () {
+        // NOTE: we do not want to stub console.log in beforeEach b/c it messes with mocha's reporting
+
+        it('should log to console', function () {
+          stub = sinon.stub(console, 'log')
+          logger.log('foo bar baz')
+          expect(console.log).to.have.been.calledWith(`${name}: foo bar baz`)
+        })
+      })
+
+      describe('when force is given', function () {
+        it('should log to console', function () {
+          stub = sinon.stub(console, 'log')
+          logger.log('foo bar baz', true)
+          expect(console.log).to.have.been.calledWith(`${name}: foo bar baz`)
+        })
+      })
     })
   })
 
   describe('.error()', function () {
-    it('should log to console', function () {
-      stub = sinon.stub(console, 'error')
-      logger.error('foo bar baz')
-      expect(console.error).to.have.been.calledWith(`${pkgJson.name}: foo bar baz`)
-      stub.restore()
+    describe('when VERBOSE is not in the env', function () {
+      beforeEach(function () {
+        delete process.env['VERBOSE']
+      })
+
+      it('should log to console', function () {
+        stub = sinon.stub(console, 'error')
+        logger.error('foo bar baz')
+        expect(console.error).to.have.been.calledWith(`${name}: foo bar baz`)
+      })
+    })
+
+    describe('when VERBOSE is in the env', function () {
+      beforeEach(function () {
+        process.env['VERBOSE'] = '1'
+      })
+
+      it('should log to console', function () {
+        stub = sinon.stub(console, 'error')
+        logger.error('foo bar baz')
+        expect(console.error).to.have.been.calledWith(`${name}: foo bar baz`)
+      })
     })
   })
 })
