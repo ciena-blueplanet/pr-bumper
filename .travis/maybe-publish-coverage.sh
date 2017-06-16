02fa808d89839e24f8107beca6e873936fcdf0fb
Adam Meadows <adam.meadows@gmail.com>
2017-06-16 10:12:20 -0600

    Update README with documentation on new config
    Still need to update the code to actually use the new config

--- .travis/maybe-publish-coverage.sh
+++ .travis/maybe-publish-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_NODE_VERSION" != "${PUBLISH_NODE_VERSION:-6.9.1}" ]
+if [ "$TRAVIS_NODE_VERSION" != "${PUBLISH_NODE_VERSION:-6.11.0}" ]
 then
   echo "Skipping coverage publish for TRAVIS_NODE_VERSION [${TRAVIS_NODE_VERSION}]"
   exit 0
--- .travis/maybe-test.sh
+++ .travis/maybe-test.sh
@@ -4,7 +4,7 @@ source $(dirname $0)/is-bump-commit.sh
 
 if isBumpCommit
 then
-  echo "Skipping install step for version bump commit"
+  echo "Skipping test step for version bump commit"
   exit 0
 fi
 
